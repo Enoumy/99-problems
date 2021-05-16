a77f426956b76a4ff97fcb2a5417909411318000
enoumy <enoumy@gmail.com>
2021-05-15 21:52:04 -0600

    Solved 19

---
+++ 19/rotate.ml
@@ -0,0 +1,17 @@
+let split list idx =
+  let rec helper i left right =
+    if i = 0 then (List.rev left), right else
+      match right with
+      | [] -> (List.rev left), right
+      | h :: t -> helper (i - 1) (h :: left) t
+  in
+  helper idx [] list
+
+let rotate l n =
+  let length = List.length l in
+  let real_n = n mod length in
+  let first, second = split l real_n in
+  second @ first
+
+let%test _ = rotate [1; 2; 3; 4] 2 = [3; 4; 1; 2]
+let%test _ = rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3 = ["d"; "e"; "f"; "g"; "h"; "a"; "b"; "c"]
